b09097f3a88ee4033e80c923258de93d8fa1489d
Ian Cooper <me@iancooper.name>
2018-11-06 00:32:30 -0500

    new post

---
+++ _posts/2018-11-05-extract-units.md
@@ -0,0 +1,128 @@
+---
+layout: post
+title: 'Text Reports II: Extract Units'
+date: 2018-11-05
+scripts: ['/assets/js/extract-units.js']
+---
+
+Building on [the first installment]({% link _posts/2018-11-01-formatting-columns.md %}), let's improve the formatting by pulling the units out of the rows and putting them in the header instead. We'll use the same data as before.
+
+## Define Patterns
+
+Using a regular expression, we can easily determine if a cell contains a numeric value with a unit and then extract both parts. Let's create a function that takes the cell contents as an input and returns the unit (or `false` if no units are found).
+
+```javascript
+// extract the units from a string
+function extractUnits(str) {
+
+    // compare with the regular expression
+    const matches = str.match(/\d+(\.\d+)?\s+(.+)/);
+
+    // was there a match?
+    if (matches !== null) {
+
+        // return the units
+        return matches[2];
+    } else {
+
+        // otherwise return false
+        return false;
+    }
+}
+```
+
+## Extract Units
+
+Now that we have a function that will parse the string and pull out the units if there are any, let's create an array that contains the units for each column (based on the first row). We are making the assumption that the units will be the same in every cell in a given column, and that the first data row exists with no blank values.
+
+```javascript
+// make a list of units based on the first row of data
+const units = grid[1].map(value => extractUnits(value));
+```
+
+Now that we've determined what the units are, we need to remove the units from the grid.
+
+```javascript
+// remove the units from the grid
+grid = grid.map((row, i) => {
+
+    // is this the header row?
+    if (i == 0) {
+
+        // don't change anything
+        return row;
+    } else {
+
+        // if we have a unit, remove it from the cell
+        return row.map((x, j) => units[j] ? x.split(/\s/)[0] : x);
+    }
+});
+```
+
+## Column Widths
+
+With the units moving to the header, we need to make sure the column widths are all wide enough. Let's update the width calculation to make sure that the units are included when we're looking at the header row words. We consider the whole unit string as a "word" in this case as we do not want to break in the middle of the unit string.
+```javascript
+// calculate the column widths
+let columnWidths = new Array(grid[0].length)
+    .fill(minimumColumnWidth)
+    .map((width, i) => Math.max(...grid.map((row, j) => {
+        
+        // is this the first row?
+        if (j === 0) {
+            // find the width of the longest word (including the units)
+            return Math.max(...row[i].split(/\b/)                      // words
+                                     .concat(units[i] ? units[i] : []) // units
+                                     .map(w => w.length));             // lengths
+        } else {
+            // find the width of the whole contents
+            return row[i].length;
+        }
+    }).concat(width)));
+```
+
+## Header
+
+Now that we've extracted the units and taken them into consideration when sizing the columns, let's update the code to print the units as part of the column headers. The units will come out on the bottom line of the header.
+
+```javascript
+// wrap the column headers based on the calculated widths
+let headers = columnWidths.map((w, i) => {
+    return grid[0][i].split(/\s+/).reduce((accumulator, current) => {
+        if (accumulator.length == 0) {
+            return [current];
+        } else {
+            const lastLine = accumulator.pop();
+            const testLine = lastLine + ' ' + current;
+            if (testLine.length <= w) {
+                return accumulator.concat(testLine);
+            } else {
+                return accumulator.concat(lastLine, current);
+            }
+        }
+    }, []).concat(units[i] ? units[i] : []); // add units
+});
+```
+
+## Output
+
+With our changes, the output now looks like this:
+
+```
+                                                 Gross           
+                                                 Caloric         
+                                      Fixed      Value at        
+                    Moisture Volatile Carbon by  Constant        
+Sample              Content  Matter   Difference Volume   Ash    
+Name    Date        wt. %    wt. %    wt. %      J/g      wt. %  
+------- ----------- -------- -------- ---------- -------- -------
+X24-03  01-Nov-2018 4.85     79.29    15.62      19985    0.25   
+X24-02  31-Oct-2018 4.52     80.91    16.01      20004    0.23   
+X24-01  30-Oct-2018 4.68     80.03    15.89      19996    0.24   
+```
+
+[You can download the complete code here.](/assets/js/extract-units.js)
+
+## Next Steps
+
+In the next iteration, we'll make the code more modular by refactoring it into some functions that can easily be reused elsewhere.
--- assets/js/extract-units.js
+++ assets/js/extract-units.js
@@ -1,14 +1,5 @@
----
-layout: post
-title: Column Formatting for Text Reports
-date: 2018-11-01
-scripts: ['/assets/js/formatting-columns.js']
----
-
-Here's the scenario: we have a system that outputs plain-text reports with data formatted into a table. Our raw data comes in JSON format; we'll use this as the input to our program:
-
-```json
-{
+// our input data
+let input = {
     "data": [
         {
             "Sample Name": "X24-03",
@@ -38,14 +29,8 @@ Here's the scenario: we have a system that outputs plain-text reports with data 
             "Ash": "0.24 wt. %"
         }
     ]
-}
-```
+};
 
-## Create Grid
-
-Using the built-in `JSON.parse()` function, we take the above JSON and interpret it as a JavaScript object called `input`. Next, we need to transform it into a simple 2-dimensional array of strings that we'll refer to as our grid. The first row of the grid is made up of the column names, and each row after that is data.
-
-```javascript
 // initialize our grid with the first row as the keys of the input data objects
 let grid = [Object.keys(input.data[0])];
 
@@ -53,15 +38,45 @@ let grid = [Object.keys(input.data[0])];
 input.data.forEach(row => {
     grid.push(grid[0].map(key => row[key]));
 });
-```
 
-## Minimum Column Widths
+// extract the units from a string
+function extractUnits(str) {
 
-Let's continue by calculating the minimum column width for each column in the grid. We'll say that we don't want any columns narrower than 7 characters wide. We also don't want to wrap any of the actual data values, so we'll break down the column headers (in `grid[0]`) into words but not the rest of the rows.
+    // compare with the regular expression
+    const matches = str.match(/\d+(\.\d+)?\s+(.+)/);
 
-```javascript
+    // was there a match?
+    if (matches !== null) {
+
+        // return the units
+        return matches[2];
+    } else {
+
+        // otherwise return false
+        return false;
+    }
+}
+
+// make a list of units based on the first row of data
+const units = grid[1].map(value => extractUnits(value));
+
+// remove the units from the grid
+grid = grid.map((row, i) => {
+
+    // is this the header row?
+    if (i == 0) {
+
+        // don't change anything
+        return row;
+    } else {
+
+        // if we have a unit, remove it from the cell
+        return row.map((x, j) => units[j] ? x.split(/\s/)[0] : x);
+    }
+});
+
 // minimum column width
-const minimumColumnWidth = 7
+const minimumColumnWidth = 7;
 
 // calculate the column widths
 let columnWidths = new Array(grid[0].length)
@@ -70,20 +85,16 @@ let columnWidths = new Array(grid[0].length)
         
         // is this the first row?
         if (j === 0) {
-            // find the width of the longest word
-            return Math.max(...row[i].split(/\b/).map(w => w.length));
+            // find the width of the longest word (including the units)
+            return Math.max(...row[i].split(/\b/)                      // words
+                                     .concat(units[i] ? units[i] : []) // units
+                                     .map(w => w.length));             // lengths
         } else {
             // find the width of the whole contents
             return row[i].length;
         }
     }).concat(width)));
-```
 
-## Print Column Headers
-
-Great, now that we have the width of each column, we can continue with outputting the column header lines. Because of the wrapping, some column headers will take more lines than others, so we'll take care to pad the header lines such that the column headers are aligned to the bottom.
-
-```javascript
 // wrap the column headers based on the calculated widths
 let headers = columnWidths.map((w, i) => {
     return grid[0][i].split(/\s+/).reduce((accumulator, current) => {
@@ -98,7 +109,7 @@ let headers = columnWidths.map((w, i) => {
                 return accumulator.concat(lastLine, current);
             }
         }
-    }, []);
+    }, []).concat(units[i] ? units[i] : []); // add units
 });
 
 // how many header lines do we need?
@@ -115,13 +126,7 @@ let lines = new Array(headerHeight).fill('').map((h, i) =>
 // create the separator lines
 lines.push(columnWidths.map(w => ''.padEnd(w, '-')).join(' '));
 
-```
 
-## Print Data Rows
-
-Printing the data rows is a bit simpler as we do not do any wrapping, although we still pad the end of each cell with spaces using `String.padEnd()` to help everything line up correctly.
-
-```javascript
 // compose each data line
 grid.forEach((row, i) => {
 
@@ -134,26 +139,3 @@ grid.forEach((row, i) => {
 
 // print out the lines
 console.log(lines.join('\n'));
-```
-
-## Output
-
-Here's what the program outputs to the console:
-
-```
-                                                       Gross               
-                                                       Caloric             
-                                           Fixed       Value at            
-Sample              Moisture   Volatile    Carbon by   Constant            
-Name    Date        Content    Matter      Difference  Volume    Ash       
-------- ----------- ---------- ----------- ----------- --------- ----------
-X24-03  01-Nov-2018 4.85 wt. % 79.29 wt. % 15.62 wt. % 19985 J/g 0.25 wt. %
-X24-02  31-Oct-2018 4.52 wt. % 80.91 wt. % 16.01 wt. % 20004 J/g 0.23 wt. %
-X24-01  30-Oct-2018 4.68 wt. % 80.03 wt. % 15.89 wt. % 19996 J/g 0.24 wt. %
-```
-
-[You can download the complete code here.](/assets/js/formatting-columns.js)
-
-## Next Steps
-
-[In the next installment]({% link _posts/2018-11-05-extract-units.md %}), we'll update the program to make the output more readable by extracting the units out of the cells and putting them in the column headers.
