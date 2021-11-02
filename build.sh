765f74d03fa36e46af1f706feb7e969010605d57
Ian Cooper <icooper@users.noreply.github.com>
2021-11-02 16:40:26 -0600

    add docker scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker run --rm -it --volume="$PWD/docs:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=production jekyll/jekyll:3.8 jekyll build
+docker run --rm -it --volume="$PWD/docs:/srv/jekyll" --volume="$PWD/docs/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=production jekyll/jekyll:3.8 jekyll build
--- serve.sh
+++ serve.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker run --rm --volume="$PWD/docs:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=development -p 4000:4000 jekyll/jekyll:3.8 jekyll serve
+docker run --rm --volume="$PWD/docs:/srv/jekyll" --volume="$PWD/docs/vendor/bundle:/usr/local/bundle" --env JEKYLL_ENV=development -p 4000:4000 jekyll/jekyll:3.8 jekyll serve
